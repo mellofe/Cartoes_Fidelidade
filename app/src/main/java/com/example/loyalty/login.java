97d55bdd7d9e89d9ff9dde3aed56e14f3cc478d3
mellofe <61694269+mellofe@users.noreply.github.com>
2020-12-13 09:49:43 -0300

    Layout transferido de home para login, e ajustes nos posicionamentos dos elementos

--- app/src/main/java/com/example/loyalty/login.java
+++ app/src/main/java/com/example/loyalty/login.java
@@ -1,6 +1,8 @@
 package com.example.loyalty;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,9 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+    public void ButtonEntrar(View view) {
+        Intent it = new Intent(getBaseContext(), home.class);
+        startActivity(it);
     }
 }
